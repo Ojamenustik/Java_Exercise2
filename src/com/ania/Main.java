00bf67b26c48efc9c1d445edfcceef8e7553d750
Ojamenustik <baka.yarichin@gmail.com>
2020-03-10 16:27:16 +0100

    Reading file with file reader, Exceptions in Java (before Java 7)

--- src/com/ania/Main.java
+++ src/com/ania/Main.java
@@ -1,8 +1,7 @@
 package com.ania;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -90,6 +89,7 @@ public class Main {
 
         Scanner in = new Scanner(textFile);
 
+        System.out.println(textFile.toString());
         int value = in.nextInt();
         System.out.println("Read value: "+ value);
 
@@ -104,7 +104,36 @@ public class Main {
         }
 
         in.close();
+    /////// Reading File with File Reader //////////
 
+        File file = new File("example.txt");
+        BufferedReader br = null;
+        try{
+            FileReader fr = new FileReader(file);
+             br = new BufferedReader(fr);
+
+            String line;
+
+            while( (line = br.readLine()) != null ){
+                System.out.println(line);
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found: " + file.toString());
+        }
+        catch (IOException e){
+            System.out.println("Unable to read file: "+ file.toString());
+        }
+        finally{
+            try{
+                br.close();
+            } catch (IOException e) {
+                System.out.println("Unable to close file: " + file.toString());
+            }
+            catch (NullPointerException ex){
+
+            }
+        }
 
     }
 }
